aea9cc078af4689b924490ea4c96033351ecd844
klmmlk <klmmlk@users.noreply.github.com>
2025-01-17 19:23:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup, Extension
+import os
 
-module = Extension('libredwg', sources=['src/libredwg.c'])
+# 指定编译输出路径
+build_dir = "build"
+
+# 配置扩展模块
+ext_modules = [
+    Extension(
+        name="libredwg",
+        sources=[os.path.join(build_dir, "src/libredwg.c")],
+        include_dirs=[os.path.join(build_dir, "include")],
+        library_dirs=[os.path.join(build_dir, "lib")],
+        libraries=["redwg"],
+    )
+]
 
 setup(
-    name='pylibredwg',
-    version='1.0',
-    description='Python bindings for libredwg',
-    ext_modules=[module],
-    py_modules=['libredwg'],
+    name="libredwg",
+    version="0.1.0",
+    ext_modules=ext_modules,
 )
