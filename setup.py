44415e2e0ee9f4a47d1255b4cf6f79a4d521469f
klmmlk <klmmlk@users.noreply.github.com>
2025-01-18 10:32:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, Extension
 import os
 
-# 指定编译输出路径
-build_dir = "build"
-
 # 配置扩展模块
 ext_modules = [
     Extension(
         name="libredwg",
-        sources=[os.path.join(build_dir, "src/libredwg.c")],
-        include_dirs=[os.path.join(build_dir, "include")],
-        library_dirs=[os.path.join(build_dir, "lib")],
-        libraries=["redwg"],
+        sources=["bindings/python/swig_python.c"],  # 修改为实际路径
+        include_dirs=["include"],  # 头文件目录
+        library_dirs=["lib"],  # 库文件目录
+        libraries=["redwg"],  # 依赖的库
     )
 ]
 
